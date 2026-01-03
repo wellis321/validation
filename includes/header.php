9d0bc0cf4e937b7ce54096215b8955ae4b0fe266
William Ellis <wellis321@msn.com>
2026-01-03 05:01:12 +0000

    added sticky

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@
     </div>
 </div>
 <!-- Navigation -->
-<nav class="bg-white shadow relative z-50">
+<nav class="bg-white shadow sticky top-0 z-50">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex items-center">
